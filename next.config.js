4f6d13de631e0e41e6d2d0b3b083242cf3626d0f
웹스토리보이 <144635651+chfhr22@users.noreply.github.com>
2024-01-17 11:18:45 +0900

    img경로 수정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isProd = process.env.NODE_ENV = 'production';
 const nextConfig = {
+    basePath: isProd ? '/green-blog' : '',
     output: 'export',
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
